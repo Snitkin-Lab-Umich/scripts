473560a411ba42a35047e3d1959e4224cb2d100c
Zena Lapp <zenalapp@flux-login1.arc-ts.umich.edu>
2018-09-12 10:54:22 -0400

    added functionality to gubbins_iqtree_raxml.sh

--- gubbins_iqtree_raxml.sh
+++ gubbins_iqtree_raxml.sh
@@ -2,6 +2,7 @@
 
 # $1 = path to fasta file
 # $2 = gubbins (1) or no gubbins (0)
+# $3 = flux account to submit to (default: esnitkin_fluxod)
 #
 # Usage:
 # Gubbins:
@@ -14,6 +15,15 @@ pref=$(echo $1 | cut -d. -f1 | rev | cut -d/ -f1 | rev)
 
 # modules to load (some of these might not be necessary)
 modules=$(echo python-anaconda2/201607 biopython fasttree dendropy reportlab RAxML raxml bioperl fastml/gub gubbins openmpi/1.10.2/gcc/4.8.5 gcc/4.8.5)
+
+# get account
+if [ $3 != '' ]; then
+  echo Will submit jobs to $3.
+  acct=$3
+else
+  echo Will submit jobs to esnitkin_flux.
+  acct=esnitkin_flux
+fi
 
 # if gubbins
 if [ $2 = 1 ]; then
@@ -31,9 +41,9 @@ echo $raxml > raxml_command.sh
   echo $iqtree > iqtree_command.sh
   
   # generate pbs scripts for gubbins, raxml, iqtree
-  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c gubbins_command.sh -o ${pref}_gubbins.pbs -M "$modules"
-  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c iqtree_command.sh -o ${pref}_iqtree.pbs -M "$modules"
-  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c raxml_command.sh -o ${pref}_raxml.pbs -M "$modules"  
+  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c gubbins_command.sh -o ${pref}_gubbins.pbs -M "$modules" -a $acct
+  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c iqtree_command.sh -o ${pref}_iqtree.pbs -M "$modules" -a $acct
+  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c raxml_command.sh -o ${pref}_raxml.pbs -M "$modules" -a $acct
 
   # start gubbins, iqtree, raxml jobs
   echo qsub ${pref}_gubbins.pbs
@@ -46,18 +56,21 @@ echo $raxml > raxml_command.sh
   raxml_qsub=$(qsub -W depend=afterok:$gub_id ${pref}_raxml.pbs)
 else
   echo Will not run gubbins.
+
+  echo Finding and removing invariant sites.
+  /nfs/esnitkin/bin_group/anaconda3/bin/snp-sites -o ${pref}_varSites.fa $1
+
   # raxml command
-  raxml=$(echo mpirun -np 2 raxmlHPC-HYBRID-SSE3 -f a -x 12345 -p 12345 -N autoMRE -m ASC_GTRGAMMA --asc-corr=lewis -s $1 -n $pref.filtered_polymorphic_sites_raxML-fa-x12345-p12345-NautoMRE-mGTRGAM-lew -T 6)
-echo $raxml > raxml_command.sh
+  raxml=$(echo mpirun -np 2 raxmlHPC-HYBRID-SSE3 -f a -x 12345 -p 12345 -N autoMRE -m ASC_GTRGAMMA --asc-corr=lewis -s ${pref}_varSites.fa -n $pref_raxML-fa-x12345-p12345-NautoMRE-mGTRGAM-lew -T 6)
+  echo $raxml > raxml_command.sh
 
   # iqtree command
-  iqtree=$(echo /nfs/esnitkin/bin_group/anaconda3/bin/iqtree -s $1 -nt AUTO -bb 1000 -m MFP -pre $pref)
+  iqtree=$(echo /nfs/esnitkin/bin_group/anaconda3/bin/iqtree -s ${pref}_varSites.fa -nt AUTO -bb 1000 -m MFP+ASC -pre ${pref}_varSites)
   echo $iqtree > iqtree_command.sh
 
-  # generate pbs scripts for gubbins, iqtree, raxml
-  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c gubbins_command.sh -o ${pref}_gubbins.pbs -M "$modules"
-  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c iqtree_command.sh -o ${pref}_iqtree.pbs -M "$modules"
-  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c raxml_command.sh -o ${pref}_raxml.pbs -M "$modules"
+  # generate pbs scripts for iqtree, raxml
+  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c iqtree_command.sh -o ${pref}_iqtree.pbs -M "$modules" -a $acct
+  /nfs/esnitkin/bin_group/anaconda3/bin/python /nfs/esnitkin/bin_group/pipeline/Github/scripts/pbs_script_maker.py -c raxml_command.sh -o ${pref}_raxml.pbs -M "$modules" -a $acct
 
   # start iqtree, raxml jobs
   echo qsub ${pref}_iqtree.pbs
