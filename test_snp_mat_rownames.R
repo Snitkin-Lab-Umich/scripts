856783028f095a308255f3f72127418c7ab33408
katiembrennan <ktbrennan3@gmail.com>
2019-06-09 17:38:29 -0400

    Created a script that will check snp matrix row names (annotations) for previously reported bugs.

---
+++ test_snp_mat_rownames.R
@@ -0,0 +1,9 @@
+# Katie Saund
+# 2019-06-09
+# Goal: this script reads in a snp_mat as created by Ali's pipeline and checks for previously reported bugs. 
+
+
+
+test_snp_mat_rownames <- function(smat){
+  
+}
