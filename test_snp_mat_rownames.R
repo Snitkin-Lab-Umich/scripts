d3e5c3438a0c3984eeba5a566bf918524356f75b
katiembrennan <ktbrennan3@gmail.com>
2019-06-09 20:26:22 -0400

    added some tests to check snp mat names

--- test_snp_mat_rownames.R
+++ test_snp_mat_rownames.R
@@ -2,8 +2,70 @@
 # 2019-06-09
 # Goal: this script reads in a snp_mat as created by Ali's pipeline and checks for previously reported bugs. 
 
+library(tidyverse)
+library(testthat)
 
+snpmat1 <-   read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv",
+                       header = TRUE,
+                       stringsAsFactors = FALSE,
+                       sep = "\t",
+                       quote = "", 
+                       row.names = 1, 
+                       nrows = 35437)
+
+snpmat2 <-   read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv",
+                       header = TRUE,
+                       stringsAsFactors = FALSE,
+                       sep = "\t",
+                       quote = "", 
+                       row.names = 1, 
+                       skip = 39000, 
+                       nrows = 500)
+
+snpmat3 <-   read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv",
+                        header = TRUE,
+                        stringsAsFactors = FALSE,
+                        sep = "\t",
+                        quote = "", 
+                        row.names = 1, 
+                        skip = 39536, 
+                        nrows = 366)
+
+snpmat4 <-   read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv",
+                        header = TRUE,
+                        stringsAsFactors = FALSE,
+                        sep = "\t",
+                        quote = "", 
+                        row.names = 1, 
+                        skip = 39907, 
+                        nrows = 351)
 
 test_snp_mat_rownames <- function(smat){
+  expect_identical(grep("ERROR", row.names(smat)), integer(0))
+  expect_identical(grep("WARNING_TRANSCRIPT_NO_START_CODON", row.names(smat)), integer(0))
+  expect_identical(grep("[[][0-9]", row.names(smat)), integer(0))
   
+  num_Non_Coding_rows <- sum((!is.na(str_match(row.names(smat), 'Non-Coding'))))
+  num_Non_Coding_rows_with_two_genes <- length(row.names(smat)[(!is.na(str_match(row.names(smat), 'Non-Coding')))] %>% gsub(".*locus_tag=", "", .) %>% gsub(" Strand Information.*", "", .) %>% grep('-', .))
+  expect_equal(num_Non_Coding_rows, num_Non_Coding_rows_with_two_genes)
+  
+  # things to test for: 
+  # 1. non-coding snps should have two genes in the locus_tag (grep for the "-" dash)
+  # 3. What does this "WARNING_TRANSCRIPT_NO_START_CODON" mean? 
 }
+
+test_snp_mat_rownames(snpmat1)
+test_snp_mat_rownames(snpmat2)
+test_snp_mat_rownames(snpmat3)
+test_snp_mat_rownames(snpmat4)
+
+
+row.names(snpmat1)[str_length(row.names(snpmat1)) > 600]
+
+row.names(snpmat1)[grep("intron", row.names(snpmat1))]
+
+row.names(snpmat1)[str_length(row.names(snpmat1)) < 200]
+
+
+row.names(snpmat1)[grep("A,T", row.names(snpmat1))]
+
