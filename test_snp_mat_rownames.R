9550e4764d2a8d61f5a9b9815b8897d97da649aa
katiembrennan <ktbrennan3@gmail.com>
2019-06-13 12:53:02 -0400

    fixed path to a functional snpmat

--- test_snp_mat_rownames.R
+++ test_snp_mat_rownames.R
@@ -5,40 +5,12 @@
 library(tidyverse)
 library(testthat)
 
-snpmat1 <-   read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv",
-                       header = TRUE,
-                       stringsAsFactors = FALSE,
-                       sep = "\t",
-                       quote = "", 
-                       row.names = 1, 
-                       nrows = 35437)
-
-snpmat2 <-   read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv",
-                       header = TRUE,
-                       stringsAsFactors = FALSE,
-                       sep = "\t",
-                       quote = "", 
-                       row.names = 1, 
-                       skip = 39000, 
-                       nrows = 500)
-
-snpmat3 <-   read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv",
-                        header = TRUE,
-                        stringsAsFactors = FALSE,
-                        sep = "\t",
-                        quote = "", 
-                        row.names = 1, 
-                        skip = 39536, 
-                        nrows = 366)
-
-snpmat4 <-   read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv",
-                        header = TRUE,
-                        stringsAsFactors = FALSE,
-                        sep = "\t",
-                        quote = "", 
-                        row.names = 1, 
-                        skip = 39907, 
-                        nrows = 351)
+snpmat <- read.table("/scratch/esnitkin_fluxod/apirani/Project_Cdiff/Analysis/Project_propensity_score_match/2019_05_14_PSM_variant_calling/core_temp_dir/test_data/SNP_matrix_allele_new.csv", 
+                   header = TRUE,
+                   stringsAsFactors = FALSE,
+                   sep = "\t",
+                   quote = "",
+                   row.names = 1)
 
 test_snp_mat_rownames <- function(smat){
   # Check that ERROR messages are not appearing
@@ -51,15 +23,21 @@ test_snp_mat_rownames <- function(smat){
   # Non-coding snps should have two genes in the locus_tag (grep for the "-" dash)
   # Maybe be too strict because of issue with intragenic variants? TBD. 
   num_Non_Coding_rows <- sum((!is.na(str_match(row.names(smat), 'Non-Coding'))))
+  num_intragenic <- sum((!is.na(str_match(row.names(smat), 'intragenic'))))
   num_Non_Coding_rows_with_two_genes <- length(row.names(smat)[(!is.na(str_match(row.names(smat), 'Non-Coding')))] %>% gsub(".*locus_tag=", "", .) %>% gsub(" Strand Information.*", "", .) %>% grep('-', .))
-  expect_equal(num_Non_Coding_rows, num_Non_Coding_rows_with_two_genes) 
+  expect_equal(num_Non_Coding_rows - num_intragenic, num_Non_Coding_rows_with_two_genes) 
   
   # What does this "WARNING_TRANSCRIPT_NO_START_CODON" mean? How should we handle it?
   expect_identical(grep("WARNING_TRANSCRIPT_NO_START_CODON", row.names(smat)), integer(0))
-
 }
 
-test_snp_mat_rownames(snpmat1)
-test_snp_mat_rownames(snpmat2)
-test_snp_mat_rownames(snpmat3)
-test_snp_mat_rownames(snpmat4)
+test_snp_mat_rownames(snpmat)
+
+
+# testing on 2019-06-11 to fix a bug. 
+temp <- row.names(snpmat)[!is.na(str_match(row.names(snpmat), 'Non-Coding'))]
+foo <- temp[is.na(str_match(temp, 'intragenic'))]
+summary(str_length(foo %>% gsub(".*locus_tag=", "", .) %>% gsub(" Strand Information.*", "", .)))
+foo[(str_length(foo %>% gsub(".*locus_tag=", "", .) %>% gsub(" Strand Information.*", "", .))) == 11]
+
+row.names(snpmat) %>% gsub(".*SNP at ", "", .) %>% gsub(" >.*", "", .) %>%  as.numeric() %>%  summary()
