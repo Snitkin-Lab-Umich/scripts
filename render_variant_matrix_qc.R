121ba65d5abc2c93b894d415f99f0ad127ccd809
Zena Lapp <zenalapp@umich.edu>
2020-01-17 09:37:27 -0500

    Update

--- render_variant_matrix_qc.R
+++ render_variant_matrix_qc.R
@@ -14,9 +14,11 @@ if(length(args) == 2){
 
 if(grepl('SNP',args[[1]],ignore.case=T)){
   pref=paste0(pref,'_SNP')
+  tmpdir = paste0('tmp_',args[[2]],'_SNP')
   title='SNP Matrix QC'
 }else if(grepl('Indel',args[[1]],ignore.case=T)){
   pref=paste0(pref,'_Indel')
+  tmpdir = paste0('tmp_',args[[2]],'_Indel')
   title='Indel Matrix QC'
 }
 
@@ -25,4 +27,7 @@ mat = normalizePath(args[[1]])
 rmarkdown::render('/nfs/esnitkin/bin_group/pipeline/Github/scripts/variant_matrix_qc.R', 
                   params=list(mat=mat,title=title),
                   output_format='html_document',
-                  output_file=paste0(pref,"_variant_matrix_qc.html"))
+                  output_file=paste0(pref,"_variant_matrix_qc.html"),
+                  intermediates_dir=tmpdir)
+
+unlink(tmpdir,recursive=T)
