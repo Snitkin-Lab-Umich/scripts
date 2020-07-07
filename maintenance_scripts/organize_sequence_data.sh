b15b82eee4a8b170e6f32967fd7e2305b1434ce0
Ali Pirani <alipirani88@gmail.com>
2020-07-07 08:28:07 -0400

    Update organize_sequence_data.sh

--- maintenance_scripts/organize_sequence_data.sh
+++ maintenance_scripts/organize_sequence_data.sh
@@ -4,4 +4,4 @@
 echo "Generating Sequence_data and its sub-directories for $USER"
 PWD=`pwd`
 echo "Generating Sequence_data directory in $PWD/"
-mkdir $PWD/fastq $PWD/metadata $PWD/Report $PWD/assembly  $PWD/assembly_annotation  $PWD/consensus  $PWD/fastq
+mkdir $PWD/fastq $PWD/metadata $PWD/Reports $PWD/assembly  $PWD/assembly_annotation  $PWD/output_files  $PWD/fastq
